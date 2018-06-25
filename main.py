fc8be4d4538964a368cc307b47aa5074ff381550
Steve <steve@lemoncloud.io>
2018-06-25 23:18:48 +0900

    initial main

---
+++ main.py
@@ -0,0 +1,13 @@
+# Main Application
+# - Run it command line.
+#
+# @auth Steve (steve@lemoncloud.io)
+#
+def main(param = None):
+    print("hello main()....")
+
+
+#############################
+# Self Test Main.
+if __name__ == '__main__':
+    main()
