e1710817b511e06421b6e86a43e278e02e72260d
Steve <steve@lemoncloud.io>
2018-07-04 01:32:34 +0900

    pools: sync-list, sync-pull

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 #
 import argparse
 import pprint
+from time import sleep
 
 '''
 ------------------------------------------------------------------------------------
@@ -17,25 +18,35 @@ def main(param = None):
     parser.add_argument('-m', '--mode', help='run mode(실행 모드: pools)', default='')
     args = vars(parser.parse_args())
     mode = args['mode']
-    mode = mode if mode else 'down-mid'
+    # mode = mode if mode else 'down-mid'
+    mode = mode if mode else 'auto-sync'
     print('! mode =', mode)
 
     # decode mode.
+    ret = None
     if mode == 'pools':
-        run_mode_pools()
+        ret = run_mode_pools()
     elif mode == 'down-mid':
-        run_mode_down_mid()
+        ret = run_mode_down_mid()
+    elif mode == 'auto-sync':
+        ret = run_mode_auto_sync()
     else:
-        parser.print_help()
+        ret = parser.print_help()
+    
+    #! print finally.
+    print('! ret =', ret);
 
 
 # mode: pools - test pools API
 def run_mode_pools():
     from tools import pools
     pools.test()
+    # eof - run_mode_pools
+    return 0
 
 # mode: down-mid - download
 def run_mode_down_mid(mid = 'NS5640996976'):
+    print('run_mode_down_mid(%s)...'%(mid))
     from tools import pools, tsv
     HEADERS = ['id','name','cat','price','delivery','mall','img']
     item = pools.describe_item(mid)
@@ -50,6 +61,85 @@ def run_mode_down_mid(mid = 'NS5640996976'):
     # load back.
     lines = tsv.load_list_from_tsv(mid)
     pprint.pprint(lines)
+
+    # eof - run_mode_down_mid
+    return 0
+
+# mode: auto-sync - list items, then populate
+def run_mode_auto_sync(page = None, total = None):
+    page = 1 if page == None else page
+    total = 0 if total == None else total
+    print('run_mode_auto_sync(%d/%d)...'%(page, total))
+    from tools import pools
+    thiz = pools.get_items(page, 'ITEM')
+    # print('> thiz=', thiz)
+    
+    # page = thiz['page']
+    list = thiz['list']
+    size = len(list)
+    print('> page=%d, size=%d'%(page, size))
+    total += size
+
+    #! do main task for each list.
+    for node in list:
+        item_id = node['id']
+        name = node['name'] if 'name' in node else ''
+        option = node['option'] if 'option' in node else None
+        hasOption = True if option else False
+        print('>> id=', item_id, hasOption, ' - ', name)
+        if not hasOption:
+            res = run_mode_sync_list_deep(item_id, 1)
+            print('>>> res=', res)
+            exit()
+
+    #! do next page.
+    if size > 0 and page < 100:
+        sleep(0.5)
+        return run_mode_auto_sync(page + 1, total)
+
+    #! return total count.
+    return {"total": total, "page": thiz['page']}
+
+
+# mode: sync-list - call `sync-list` by page.
+def run_mode_sync_list(item_id, page = None, total = None):
+    page = 1 if page == None else page
+    total = 0 if total == None else total
+    print('run_mode_sync_list(%s, %d/%d)...'%(item_id, page, total))
+    from tools import pools
+    thiz = pools.sync_list_item(item_id, page)
+    # page = thiz['page']
+    list = thiz['list'] if 'list' in thiz else []
+    size = len(list)
+    print('> page=%d, size=%d'%(page, size))
+    total += size
+    #! return
+    return {"total": total, "page": thiz['page'], "size": size, "list": list}
+
+# mode: sync-list-deep - call `sync-list` in deep until eof
+def run_mode_sync_list_deep(item_id, page = 1, total = 0):
+    from tools import pools
+    print('run_mode_sync_list_deep(%s, %d/%d)...'%(item_id, page, total))
+    for i in range(page, 100):            # max - 100 page
+        res = run_mode_sync_list(item_id, i, total)
+        list = res['list']
+        total = res['total']
+        size = res['size']
+
+        # for each mall. call sync-pull.
+        for i, mall in enumerate(list):
+            sleep(2)
+            mid = mall['id']
+            res2 = pools.sync_pull_item(mid)
+            print('%03d:%02d> sync[%s] ='%(page, i, mid), res2)
+
+        # check eof.
+        if (size < 20):
+            page = i
+            break
+        
+    #! return
+    return {"total": total, "page": page, "size": 0}
 
 
 #############################
--- tools/pools.py
+++ tools/pools.py
@@ -25,20 +25,42 @@ def _url(path):
     return BASE_URL + path
 
 #! get items : {list, page, total}
-def get_items():
-    res = requests.get(_url('/item-pools/'), {'ns':'NS'})
+#  - 검색을 시전함.
+def get_items(page = None, type = None):
+    param = {'ns':'NS', 'invalid':0}
+    if (type != None): param['type'] = type
+    if (page != None): param['page'] = page
+    res = requests.get(_url('/item-pools/'), param)
     if res.status_code != 200:
         raise ApiError('Get Lists Error: {}'.format(res.status_code))
     return res.json()
 
 #! get detail of item
+#  - 상세 정보를 얻어옴.
 def describe_item(item_id):
     res = requests.get(_url('/item-pools/{:s}/'.format(item_id)))
     if res.status_code != 200:
         raise ApiError('Get Item: {}'.format(res.status_code))
     return res.json()
 
+#! call sync-list for item-id (ex: NS8159761521)
+#  - ITEM 에 하위 MALL의 목록을 가져옴.
+def sync_list_item(item_id, page = 1):
+    res = requests.get(_url('/item-pools/{:s}/sync-list?page={:d}'.format(item_id, page)))
+    if res.status_code != 200:
+        raise ApiError('Get Item: {}'.format(res.status_code))
+    return res.json()
+
+#! call sync-pull for item-id (ex: NS8159761521)
+#  - 각 MALL 에 대한 상세 정보 (이미지 포함)을 업데이트함.
+def sync_pull_item(item_id):
+    res = requests.get(_url('/item-pools/{:s}/sync-pull'.format(item_id)))
+    if res.status_code != 200:
+        raise ApiError('Get Item: {}'.format(res.status_code))
+    return res.json()
+
 #! update fields.
+#  - 내부 속성을 업데이트 함.
 def update_item(item_id, description):
     url = _url('/item-pools/{:s}/'.format(item_id))
     res = requests.put(url, json= {'description': description})
